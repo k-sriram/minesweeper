9a566e2c566e82cfe02a295b3956bf4ce454c979
Sriram Krishna <ksri91@gmail.com>
2022-06-01 19:30:14 +0530

    ✅ Implementing basic functionality of Game

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,17 @@
-use std::fmt::Display;
+use std::{
+    cmp::{max, min},
+    fmt::Display,
+};
 
 pub trait Randomizer {
-    fn generate_board(&self, width: u32, height: u32, num_mines: u32) -> Vec<Vec<bool>>;
+    fn generate_board(
+        &self,
+        width: u32,
+        height: u32,
+        num_mines: u32,
+        firstx: u32,
+        firsty: u32,
+    ) -> Vec<Vec<bool>>;
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -51,6 +61,7 @@ pub struct Game<R: Randomizer> {
     height: u32,
     num_mines: u32,
     randomizer: R,
+    clear_remaining: u32,
     mines: Vec<Vec<bool>>,
     opened: Vec<Vec<bool>>,
     neighbours: Vec<Vec<u8>>,
@@ -58,29 +69,66 @@ pub struct Game<R: Randomizer> {
 
 impl Game<DefaultRandomizer> {
     pub fn new(width: u32, height: u32, num_mines: u32) -> Self {
-        todo!()
+        Game::<DefaultRandomizer>::new_with(height, width, num_mines, DefaultRandomizer {})
     }
 }
 
 impl<R: Randomizer> Game<R> {
-    pub fn new_with(width: u32, height: u32, num_mines: u32, randomizer: R) -> Game<R> {
-        todo!()
+    pub fn new_with(width: u32, height: u32, num_mines: u32, randomizer: R) -> Self {
+        if width < 1 || height < 1 {
+            panic!("Invalid size");
+        }
+        if num_mines < 1 || num_mines > (width * height - 1) {
+            panic!("Invalid number of mines");
+        }
+        Self {
+            state: GameState::New,
+            width,
+            height,
+            num_mines,
+            randomizer,
+            clear_remaining: 0,
+            mines: Vec::new(),
+            opened: Vec::new(),
+            neighbours: Vec::new(),
+        }
     }
 
     pub fn clear(&mut self) {
-        todo!()
+        self.state = GameState::New;
+        self.mines = Vec::new();
+        self.opened = Vec::new();
+        self.neighbours = Vec::new();
     }
 
     pub fn reset(&mut self, width: u32, height: u32, num_mines: u32) {
-        todo!()
+        self.height = height;
+        self.width = width;
+        self.num_mines = num_mines;
+        self.clear();
+    }
+
+    pub fn reset_randomizer(&mut self, randomizer: R) {
+        self.randomizer = randomizer;
+        self.clear();
     }
 
     pub fn get_state(&self) -> GameState {
-        todo!()
+        self.state
     }
 
     pub fn get_cell(&self, x: u32, y: u32) -> Cell {
-        todo!()
+        match self.state {
+            GameState::New => Cell::Hidden,
+            GameState::Playing if !self.opened[y as usize][x as usize] => Cell::Hidden,
+            _ => {
+                if self.mines[y as usize][x as usize] {
+                    Cell::Mine
+                } else {
+                    Cell::Clear(self.neighbours[y as usize][x as usize])
+                }
+            }
+        }
     }
 
     pub fn get_board(&self) -> Vec<Vec<Cell>> {
@@ -88,14 +136,80 @@ impl<R: Randomizer> Game<R> {
     }
 
     pub fn open(&mut self, x: u32, y: u32) -> Result<OpenInfo, OpenErr> {
-        todo!()
+        if x >= self.width || y >= self.height {
+            return Err(OpenErr::OutOfBounds);
+        }
+        match self.state {
+            GameState::New => {
+                self.state = GameState::Playing;
+                self.generate_board(x, y);
+                self.open(x, y)
+            }
+            GameState::Playing => {
+                if self.opened[y as usize][x as usize] {
+                    Err(OpenErr::AlreadyOpen)
+                } else {
+                    self.opened[y as usize][x as usize] = true;
+                    if self.mines[y as usize][x as usize] {
+                        self.state = GameState::Lost;
+                        Ok(OpenInfo {
+                            state: self.state,
+                            cell: Cell::Mine,
+                        })
+                    } else {
+                        self.clear_remaining -= 1;
+                        if self.clear_remaining == 0 {
+                            self.state = GameState::Won;
+                        }
+                        Ok(OpenInfo {
+                            state: self.state,
+                            cell: Cell::Clear(self.neighbours[y as usize][x as usize]),
+                        })
+                    }
+                }
+            }
+            _ => Err(OpenErr::GameOver),
+        }
+    }
+
+    // Utility functions
+
+    fn generate_board(&mut self, firstx: u32, firsty: u32) {
+        self.mines =
+            self.randomizer
+                .generate_board(self.width, self.height, self.num_mines, firstx, firsty);
+        self.opened = vec![vec![false; self.width as usize]; self.height as usize];
+        self.calculate_neighbours();
+        self.clear_remaining = self.width * self.height - self.num_mines;
+    }
+
+    fn calculate_neighbours(&mut self) {
+        self.neighbours = vec![vec![0; self.width as usize]; self.height as usize];
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.mines[y as usize][x as usize] {
+                    for j in max(1, y as usize) - 1..min(self.height, y + 2) as usize {
+                        for i in max(1, x as usize) - 1..min(self.width, x + 2) as usize {
+                            self.neighbours[j][i] += 1;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 pub struct DefaultRandomizer {}
 
 impl Randomizer for DefaultRandomizer {
-    fn generate_board(&self, width: u32, height: u32, num_mines: u32) -> Vec<Vec<bool>> {
+    fn generate_board(
+        &self,
+        width: u32,
+        height: u32,
+        num_mines: u32,
+        firstx: u32,
+        firsty: u32,
+    ) -> Vec<Vec<bool>> {
         todo!()
     }
 }
@@ -107,7 +221,14 @@ mod tests {
     struct DummyRandomizer {}
 
     impl Randomizer for DummyRandomizer {
-        fn generate_board(&self, width: u32, height: u32, num_mines: u32) -> Vec<Vec<bool>> {
+        fn generate_board(
+            &self,
+            width: u32,
+            height: u32,
+            num_mines: u32,
+            _firstx: u32,
+            _firsty: u32,
+        ) -> Vec<Vec<bool>> {
             let mut mines = vec![vec![false; width as usize]; height as usize];
             for i in 0..num_mines {
                 let x = i % width;
@@ -167,7 +288,7 @@ mod tests {
         );
 
         assert_eq!(game.get_state(), GameState::Won);
-        assert_eq!(game.open(0, 1), Err(OpenErr::GameOver));
+        assert_eq!(game.open(1, 0), Err(OpenErr::GameOver));
 
         game.clear();
         assert_eq!(game.get_state(), GameState::New);
